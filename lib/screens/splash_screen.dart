164a90cb27667a58e6e2a483922800b4463a12a6
km-talha <khandokerabutalha6@gmail.com>
2026-02-14 15:50:04 +0600

    Create splash_screen.dart file with animated splash screen for meal app

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Navigate after 3 seconds
+    Future.delayed(Duration(seconds: 3), () {
+      // Replace with your navigation code
+      Navigator.of(context).pushReplacementNamed('/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/logo.png', height: 100), // Replace with your logo path
+            SizedBox(height: 20),
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
